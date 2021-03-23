dd66115c1a1f498362a605a0d882898cc3618477
南七 <201971010259@nwnu.edu.cn>
2021-03-23 20:24:43 +0800

    first demo

--- src/main/java/com/demo/firstdemo/HelloServlet.java
+++ src/main/java/com/demo/firstdemo/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World 2021 ！";
+        message = "Hello World ！";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
